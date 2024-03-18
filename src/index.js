03512015a2cf9605567a0cc9a10c2a62a69c47f3
Mayank Kumar Gupta <66107976+mkgupta01@users.noreply.github.com>
2024-03-18 12:41:20 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/bhfl", (req, res) => {
+  const arr = req.body;
+  const data = {
+    name: "Mayank Kumar Gupta",
+    email: "mayank1946.be21@chitkara.edu.in",
+    roll_number: 2110991946,
+    odd_number: [],
+    even_number: [],
+    alphabet: [],
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      data.even_number.push(arr[i]);
+    } else {
+      data.odd_number.push(arr[i]);
+    }
+    if (arr[i] >= 65 && arr[i] <= 90) {
+      data.alphabet.push(arr[i]);
+    }
+  }
+  res.status(200).json({
+    is_success: true,
+    data: data,
+  });
+});
+
+app.listen(8080, function () {
+  console.log("Server is running on 8080");
+});
